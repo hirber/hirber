9e2ab609913665b5f9a465ed506763326457c126
Aleksander Długopolski <ad@dotpro.org>
2020-10-09 15:18:51 +0200

    Add basic example spec for RSpec

---
+++ spec/views_spec.rb
@@ -0,0 +1,48 @@
+require "spec_helper"
+require "hirb"
+
+Hirb.enable
+
+describe "activerecord table" do
+  context "with no select" do
+    let(:pet) do
+      double(
+        name: "rufus",
+        age: 7,
+        attributes: { "name" => "rufus", "age" => 7 },
+        class: double(column_names: %w{age name}),
+      )
+    end
+
+    it "gets default options" do
+      expect(Hirb::Helpers::AutoTable.active_record__base_view(pet))
+        .to eq(fields: [:age, :name])
+    end
+  end
+
+  context "with select" do
+    let(:pet) do
+      double(
+        name: "rufus",
+        age: 7,
+        attributes: { "name" => "rufus" },
+        class: double(column_names: %w{age name}),
+      )
+    end
+
+    it "gets default options" do
+      expect(Hirb::Helpers::AutoTable.active_record__base_view(pet))
+        .to eq(fields: [:name])
+    end
+  end
+end
+
+describe "mongoid table" do
+  let(:mongoid_stub) { double(class: double(fields: fields)) }
+  let(:fields) { {"_id" => "x0f0x", "name" => "blah"} }
+
+  it "only has one _id" do
+    expect(Hirb::Helpers::AutoTable.mongoid__document_view(mongoid_stub))
+      .to eq(fields: fields.keys.sort)
+  end
+end
