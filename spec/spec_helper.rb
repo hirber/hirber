5d043c0c3cb4cf0d13954a265340197899d7f7ab
Aleksander Długopolski <ad@dotpro.org>
2021-02-14 17:49:11 +0100

    Add helpers to spec_helper
    This should be moved to a separate file later on.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,41 @@
+module RSpec::Helpers
+  ENV["LINES"] = ENV["COLUMNS"] = "20"
+
+  def reset_config
+    Hirb::View.instance_eval do
+      @config = nil
+    end
+  end
+
+  def capture_stderr(&block)
+    original_stderr = $stderr
+    $stderr = fake = StringIO.new
+    begin
+      yield
+    ensure
+      $stderr = original_stderr
+    end
+    fake.string
+  end
+
+  def reset_terminal_size
+    ENV["LINES"] = ENV["COLUMNS"] = "20"
+  end
+end
+
+module ::IRB
+  class Irb
+    def initialize(context)
+      @context = context
+    end
+
+    def output_value; end
+  end
+end
+
 RSpec.configure do |config|
+  include RSpec::Helpers
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -7,6 +44,7 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.bisect_runner = :shell
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.filter_run_when_matching :focus
   config.example_status_persistence_file_path = "spec/examples.txt"
