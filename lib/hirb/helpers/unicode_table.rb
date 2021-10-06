e996a39a8eae6ae9ea694eb7b6d41964e0c93266
Marz Drel <marzdrel@users.noreply.github.com>
2021-10-06 10:14:38 +0200

    Fix style issues (formatting) in table definitions

--- lib/hirb/helpers/unicode_table.rb
+++ lib/hirb/helpers/unicode_table.rb
@@ -1,12 +1,35 @@
 # -*- encoding : utf-8 -*-
 class Hirb::Helpers::UnicodeTable < Hirb::Helpers::Table
   CHARS = {
-    :top => {:left => '┌', :center => '┬', :right => '┐', :horizontal => '─',
-      :vertical => {:outside => '│', :inside => '│'} },
-    :middle => {:left => '├', :center => '┼', :right => '┤', :horizontal => '─'},
-    :bottom => {:left => '└', :center => '┴', :right => '┘', :horizontal => '─',
-      :vertical => {:outside => '│', :inside => '╎'} }
-  }
+    :top => {
+      :left => "┌",
+      :center => "┬",
+      :right => "┐",
+      :horizontal => "─",
+      :vertical => {
+        :outside => "│",
+        :inside => "│",
+      },
+    },
+
+    :middle => {
+      :left => "├",
+      :center => "┼",
+      :right => "┤",
+      :horizontal => "─",
+    },
+
+    :bottom => {
+      :left => "└",
+      :center => "┴",
+      :right => "┘",
+      :horizontal => "─",
+      :vertical => {
+        :outside => "│",
+        :inside => "╎",
+      },
+    },
+  }.freeze
 
   # Renders a unicode table
   def self.render(rows, options={})
